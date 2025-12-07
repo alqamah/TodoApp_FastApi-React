bc5dd09d8889cf30fab29d5150a4203a5165a38d
alqamah <alqama.hasnain@gmail.com>
2025-12-07 16:41:44 +0530

    write implemented

--- vite-project/src/components/Todos.tsx
+++ vite-project/src/components/Todos.tsx
@@ -27,8 +27,44 @@ const TodosContext = createContext({
   todos: [], fetchTodos: () => { }
 })
 
+//write
+function AddTodo() {
+  const [item, setItem] = React.useState("")
+  const {todos, fetchTodos} = React.useContext(TodosContext)
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItem(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const newTodo = {
+      "id": todos.length + 1,
+      "item": item
+    }
+    //backend write
+    fetch("http://localhost:8000/todo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo)
+    }).then(fetchTodos)
+  }
+  return (
+  <form onSubmit={handleSubmit}>
+    <Input
+      pr="4.5rem"
+      type="text"
+      placeholder="Add a todo item"
+      aria-label="Add a todo item"
+      onChange={handleInput}
+    />
+  </form>
+  )
+}
+
+//main component
 export default function Todos() {
   const [todos, setTodos] = useState([])
+  //read
   const fetchTodos = async () => {
     const response = await fetch("http://localhost:8000/todo")
     const todos = await response.json()
@@ -39,8 +75,9 @@ export default function Todos() {
   }, [])
 
   return (
-    <TodosContext.Provider value={{ todos, fetchTodos }}>
+    <TodosContext.Provider value={{todos, fetchTodos}}>
       <Container maxW="container.xl" pt="100px">
+      <AddTodo />  {/*add todo container */}
         <Stack gap={5}>
           {todos.map((todo: Todo) => (
             <b key={todo.id}>{todo.item}</b>
